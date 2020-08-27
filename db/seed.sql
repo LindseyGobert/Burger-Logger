67847e5c1b001555589f4d429f63d13330b520fb
Lindsey Gobert <lmgobert1@gmail.com>
2020-08-26 23:52:22 -0500

    added code to the config, controller, db, and models folders

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Shrimp Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burgers');
